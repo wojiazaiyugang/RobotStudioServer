5f58c2d65c0464c70274d7cc77cb1466c218bca7
wojiazaiyugang <13061044@buaa.edu.cn>
2020-09-04 22:04:08 +0800

    读写ini文件接口稍微更新一下

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from communication import get_msg, send_msg
-from file import get_ini_config
+from file import set_all_config, get_all_config
 
 if __name__ == '__main__':
     while True:
         command = get_msg()
         # print(command)
-        # func, *args = command.split("|")
-        # result = eval(f"{func}(args)")
-        # send_msg(result)
-        send_msg(command)
+        func, *args = command.split("|")
+        try:
+            result = eval(f"{func}(args)") or "ok"
+            send_msg(result)
+        except ValueError:
+            raise ValueError
